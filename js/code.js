1125983e1e84ebada2b76f5ca8828272d9fb0f03
Arkatch <32023460+Arkatch@users.noreply.github.com>
2018-02-07 13:20:23 +0100

    ver v2
    less code

--- js/code.js
+++ js/code.js
@@ -1,9 +1,8 @@
 window.onload = ()=>{
-	create()
-}
-async function create(){
-	let data = await apiAsync()
-	buildExt(data)
+	fetch('https://api.coinmarketcap.com/v1/ticker/?limit=15')
+    .then(resp => resp.json())
+	.then(resp => build(resp))
+	.then(resp => buildExt(resp))
 }
 function buildExt(data){
 	let main = htmlElem('div', [['id', 'main']])
@@ -23,81 +22,28 @@ function htmlElem(tag, attribute = []){
 	}
 	return x
 }
-var coinList = new Map()
-function ajax(url){
-	return new Promise((resolve, reject)=>{
-		var request = new XMLHttpRequest()
-		request.onload = ()=>{
-			if(request.status == 200){
-				resolve(request.responseText)
-			}else{
-				reject(request.statusText)
-			}
-		}
-		request.onerror = ()=>{
-		  reject(request.statusText)
-		}
-		request.open("GET", url, true)
-		request.send()
-	})
-}
-async function apiAsync(){
-	try{
-		let apiResponse = await ajax('https://api.coinmarketcap.com/v1/ticker/?limit=15')
-		return build(apiResponse)
-	}catch(e){
-		console.log(e)
-	}
-}
-class Coin{
-	constructor(name, price, proc_1h, proc_24h, proc_7d, symbol){
-		this.name 		= name
-		this.priceUsd 	= price
-		this.proc_1h 	= proc_1h
-		this.proc_24h 	= proc_24h
-		this.proc_7d	= proc_7d
-		this.symbol 	= symbol
-	}
-	get info(){
-		return this.contact()
-	}
-	contact(){
-		return this.symbol+' $'+this.formatUSD()
-	}
-	formatUSD(){
-		let x = parseFloat(this.priceUsd)	//1033,34
-		if(x>=1000){
-			let temp = Math.floor(x/1000) 	//1033,34/1000 -> 1
-			let frac = x-Math.floor(x)		//1033,34 - 1033 -> 0,34
-			x = x-(temp*1000)-frac			//1033,34 - 1000 - 0,34 -> 33
-			
-			if(x<100)
-				x = '0'+x
-			
-			let newFrac = frac*10 				//0,95 -> 9,5
-			let oldFrac = Math.floor(frac*10) 	//0,95 -> 9,5 -> 9
-			if(newFrac%oldFrac)
-				frac = parseInt(frac*100)
-			else
-				frac = parseInt(frac*10)+'0'
-			
-			x = temp+' '+x+','+frac
-		}else{
-			let temp = Math.floor(x)
-			let frac = Math.round(x*100)-(temp*100)
-			x = temp+','+frac
-		}
-		return x
-	}
-}
 function build(data){
-	let cryptoData = JSON.parse(data)
 	let dataRet = []
-	for(let i of cryptoData){
-		coinList.set(i['name'], new Coin(i['name'], i['price_usd'], i['percent_change_1h'], i['percent_change_24h'], i['percent_change_7d'], i['symbol']))
-	}
-	for(let key of coinList.keys()){
-		dataRet.push(coinList.get(key).info)
+	for(let i of data){	
+		dataRet.push(i['symbol']+' '+usdFormat(i['price_usd']))
 	}
 	return dataRet
+}
+function usdFormat(price){
+	let priceFinal = '$'
+	let tab = price.split('.')
+	let tmp1 = tab[0].split('')
+	let tmp2 = tab[1].split('')
+	
+	let len1 = tmp1.length
+	let len2 = tmp2.length
+	
+	if(len1 > 3){
+		tmp1.splice(len1-3, 0, ' ')	
+	}
+	if(len2 > 2){
+		tmp2.splice(3, len2);
+	}
+	priceFinal += tmp1.join('')+'.'+tmp2.join('')
+	return priceFinal
 }
